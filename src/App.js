d05e8a7058611bf10888ca39d29db48746d551a4
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-10-13 08:08:30 +0530

    update: firebase otp auth on react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import React, { useState } from "react";
+import auth from "./firebase/firebase";
 
 function App() {
+  const [otp, setOtp] = useState("");
+  const [showOtp, setShowOtp] = useState(false);
+
+  const handleClick = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "normal",
+        callback: (response) => {
+          console.log(response);
+        },
+      },
+      auth
+    );
+    handleSignIn();
+  };
+
+  const handleSignIn = () => {
+    const phoneNumber = "+94779746304";
+    const appVerifier = window.recaptchaVerifier;
+
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        console.log(confirmationResult);
+        window.confirmationResult = confirmationResult;
+        setShowOtp(true);
+        appVerifier.clear();
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+      })
+      .catch((error) => {
+        console.log(error);
+        // Error; SMS not sent
+        // ...
+        
+      });
+  };
+
+  const handleOtp = () => {
+    window.confirmationResult
+      .confirm(otp)
+      .then((result) => {
+        // User signed in successfully.
+        const user = result.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        // ...
+        console.log(error);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex align={"center"} justify="center" flexDir={"column"}>
+      <Button onClick={handleClick}>Click Here</Button>
+      <Box id="recaptcha-container"></Box>
+      {showOtp && (
+        <Box>
+          <FormControl>
+            <FormLabel>OTP</FormLabel>
+            <Input
+              type="text"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+            />
+            <Button onClick={handleOtp}>Verify</Button>
+          </FormControl>
+        </Box>
+      )}
+    </Flex>
   );
 }
 
